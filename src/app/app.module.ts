2d63d7bfa97d9a6783f4c8b983fdb0271d725d97
maartenschroons <r0761272@student.thomasmore.be>
2019-11-08 11:36:55 +0100

    Add Components, Modules and Models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import {SharedModule} from './shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { AddCharacterComponent } from './add-character/add-character.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'list', component: CharacterListComponent },
+  { path: 'add', component: AddCharacterComponent },
+  { path: 'user', component: UserListComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CharacterListComponent,
+    AddCharacterComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
