0eb3f968d8c36c5a385f0360fc80f48922b9b9b3
maartenschroons <r0761272@student.thomasmore.be>
2019-11-15 20:23:31 +0100

    Modified Equipment and Class Services

--- src/app/add-character/add-character.module.ts
+++ src/app/add-character/add-character.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { DefaultValueAccessor } from '@angular/forms';
 
 
 
@@ -8,7 +9,8 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    DefaultValueAccessor
   ]
 })
 export class AddCharacterModule { }
